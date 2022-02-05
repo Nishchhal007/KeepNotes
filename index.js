e4a3918fc2ec1bd130e7c5d5e6a2c742a5911a6b
Nishchhal <nishchhal901@gmail.com>
2022-02-05 16:08:30 +0530

    Added Title Successfully

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ showNotes();
 let add  = document.getElementById('add');
 add.addEventListener("click",function (e) {
     let content = document.getElementById('description');
+    let title = document.getElementById('title');
     let notes = localStorage.getItem("notes");
     if (notes==null) {
         notesObj = [];
@@ -10,13 +11,17 @@ add.addEventListener("click",function (e) {
     else{
         notesObj = JSON.parse(notes);
     }
-    notesObj.push(content.value);
+    let myObj = {
+        cardtitle : title.value,
+        text : content.value
+    }
+    notesObj.push(myObj);
     localStorage.setItem("notes",JSON.stringify(notesObj));
     content.value = "";
+    title.value = "";
     // console.log(notesObj);
 
     showNotes();
-    // let title = document.getElementById('title');
 })
 
 
@@ -32,8 +37,8 @@ function showNotes(element,index) {
       html += `
               <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
                       <div class="card-body">
-                          <h5 class="card-title">Note ${index + 1}</h5>
-                          <p class="card-text"> ${element}</p>
+                          <h5 class="card-title">${element.cardtitle}</h5>
+                          <p class="card-text"> ${element.text}</p>
                           <button id = "${index}" onclick="deleteNote(this.id)" class="btn btn-danger">Delete Note</button>
                       </div>
                   </div>`;
@@ -64,7 +69,9 @@ function deleteNote(index) {
 let search = document.getElementById('search');
 search.addEventListener("input",function () {
     let inputTxt = search.value.toLowerCase();
+
     let noteCard = document.getElementsByClassName('noteCard');
+    
     Array.from(noteCard).forEach(element => {
         let cardtxt = element.getElementsByTagName("p")[0].innerText;
         if (cardtxt.includes(inputTxt)) {
